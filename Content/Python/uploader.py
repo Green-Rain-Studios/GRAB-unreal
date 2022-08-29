eafe357768be758a0c655ab6c72fa21e45ccb59f
Aashish Vasudevan <aashish@greenrain.io>
2022-08-29 14:29:57 +0530

    Switched out all keys user specific data to utilize plugin settings data

--- Content/Python/uploader.py
+++ Content/Python/uploader.py
@@ -1,14 +1,13 @@
 import unreal
-import unreal.GRABBPLibrary as grab
 import os
 import boto3
 
 session = boto3.session.Session()
 client = session.client('s3',
-                        region_name='sgp1',
-                        endpoint_url='https://sgp1.digitaloceanspaces.com',
-                        aws_access_key_id=grab.get_api_key(),
-                        aws_secret_access_key=grab.get_api_secret())
+                        region_name=unreal.GRABBPLibrary.get_bucket_region(),
+                        endpoint_url=unreal.GRABBPLibrary.get_storage_endpoint(),
+                        aws_access_key_id=unreal.GRABBPLibrary.get_api_key(),
+                        aws_secret_access_key=unreal.GRABBPLibrary.get_api_secret())
 
 # Upload a file to bucket at path
 def upload_asset_previews(name:str, path:str):
@@ -18,16 +17,16 @@ def upload_asset_previews(name:str, path:str):
     video_local_path = unreal.Paths.combine([local_path, name+'.mp4'])
 
     # Strip path of the `/Game/` prefix
-    path = path.replace('/Game/', '')
+    key_path = path.replace('/Game/', '')
 
     # URL keys
-    thumbnail_key = path + '/' + name + '.jpeg'
-    video_key = path + '/' + name + '.mp4'
+    thumbnail_key = key_path + '/' + name + '.jpeg'
+    video_key = key_path + '/' + name + '.mp4'
 
     # Upload thumbnail
     with open(thumbnail_local_path, "rb") as img:
         client.put_object(
-                            Bucket='grab-bucket',
+                            Bucket=unreal.GRABBPLibrary.get_bucket_name(),
                             Key=thumbnail_key,
                             Body=img,
                             ACL='public-read'
@@ -36,13 +35,13 @@ def upload_asset_previews(name:str, path:str):
     # Upload video
     with open(video_local_path, "rb") as vid:
         client.put_object(
-                            Bucket='grab-bucket',
+                            Bucket=unreal.GRABBPLibrary.get_bucket_name(),
                             Key=video_key,
                             Body=vid,
                             ACL='public-read'
                         )
 
-    thumbnail_url = 'https://grab-bucket.sgp1.digitaloceanspaces.com'+'/'+thumbnail_key
-    video_url = 'https://grab-bucket.sgp1.digitaloceanspaces.com'+'/'+video_key
+    thumbnail_url = unreal.GRABBPLibrary.get_bucket_url()+'/'+thumbnail_key
+    video_url = unreal.GRABBPLibrary.get_bucket_url()+'/'+video_key
 
     return thumbnail_url, video_url
